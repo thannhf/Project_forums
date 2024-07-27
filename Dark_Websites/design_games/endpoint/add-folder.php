692bac0dff46dc95ed94f1010b74e53be81ca18a
thannhf <112296523+thannhf@users.noreply.github.com>
2024-07-27 15:56:47 +0000

    update design_games

--- Dark_Websites/design_games/endpoint/add-folder.php
+++ Dark_Websites/design_games/endpoint/add-folder.php
@@ -1,32 +1,27 @@
 <?php 
-include("../conn/conn.php");
+    include("../conn/conn.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["folder_name"])) {
-        $folderName = $_POST["folder_name"];
-        
-        try {
-            $stmt = $conn->prepare("INSERT INTO tbl_image_folder (folder_name) VALUES (:folder_name)");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST["folder_name"])) {
+            $folderName = $_POST["folder_name"];
             
-            $stmt->bindParam(":folder_name", $folderName);
+            try {
+                $stmt = $conn->prepare("INSERT INTO tbl_image_folder (folder_name) VALUES (:folder_name)");
+                $stmt->bindParam(":folder_name", $folderName);
+                $stmt->execute();
 
-            $stmt->execute();
-
-            echo"
-            <script>
-                alert('Folder Added Successfully');
-                window.location.href = 'http://localhost/Dark_Websites/design_games/index.php';
-            </script>
-            ";
-            exit();
-        } catch (PDOException $e) {
-            echo "Error: ". $e->getMessage();
+                echo"
+                <script>
+                    alert('Folder Added Successfully');
+                    window.location.href = 'http://localhost/Dark_Websites/design_games/index.php';
+                </script>
+                ";
+                exit();
+            } catch (PDOException $e) {
+                echo "Error: ". $e->getMessage();
+            }
+        
+        
         }
-    
-    
     }
-}
-
-
-
 ?>
--- Dark_Websites/design_games/endpoint/add-image.php
+++ Dark_Websites/design_games/endpoint/add-image.php
@@ -1,38 +1,40 @@
 <?php
-include("../conn/conn.php");
+    include("../conn/conn.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $folder_id = $_POST['tbl_image_folder_id'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $folder_id = $_POST['tbl_image_folder_id'];
 
-    // Check if files were uploaded
-    if (isset($_FILES['image_name']['name'])) {
-        $image_name = $_FILES['image_name']['name'];
-        $image_tmp = $_FILES['image_name']['tmp_name'];
-        $image_size = $_FILES['image_name']['size'];
-        $image_type = $_FILES['image_name']['type'];
+        // Check if files were uploaded
+        if (isset($_FILES['image_name']['name'])) {
+            $image_name = $_FILES['image_name']['name'];
+            $image_tmp = $_FILES['image_name']['tmp_name'];
+            $image_size = $_FILES['image_name']['size'];
+            $image_type = $_FILES['image_name']['type'];
 
-        $target_directory = '../images/'; // Change this path as needed
+            $target_directory = '../images/'; // Change this path as needed
 
-        $unique_filename = uniqid() . '_' . $image_name;
+            // $unique_filename = uniqid() . '_' . $image_name;
+            $random_bytes = bin2hex(random_bytes(8)); // 16 ký tự ngẫu nhiên
+            $unique_filename = $random_bytes . '_' . uniqid('', true) . '.' . pathinfo($image_name, PATHINFO_EXTENSION);
 
-        if (!is_dir($target_directory)) {
-            mkdir($target_directory, 0777, true);
-        }
+            if (!is_dir($target_directory)) {
+                mkdir($target_directory, 0777, true);
+            }
 
-        if (move_uploaded_file($image_tmp, $target_directory . $unique_filename)) {
+            if (move_uploaded_file($image_tmp, $target_directory . $unique_filename)) {
 
-            $stmt = $conn->prepare("INSERT INTO tbl_image (tbl_image_folder_id, image_name) VALUES (:folder_id, :unique_filename)");
-            $stmt->bindParam(':folder_id', $folder_id);
-            $stmt->bindParam(':unique_filename', $unique_filename);
-            $stmt->execute();
+                $stmt = $conn->prepare("INSERT INTO tbl_image (tbl_image_folder_id, image_name) VALUES (:folder_id, :unique_filename)");
+                $stmt->bindParam(':folder_id', $folder_id);
+                $stmt->bindParam(':unique_filename', $unique_filename);
+                $stmt->execute();
 
-            header('Location: http://localhost/http://localhost/Dark_Websites/design_games/index.php');
+                header('Location:http://localhost/Dark_Websites/design_games/index.php');
+            } else {
+
+                echo "Error uploading the image.";
+            }
         } else {
-
-            echo "Error uploading the image.";
+            echo "Please select an image to upload.";
         }
-    } else {
-        echo "Please select an image to upload.";
     }
-}
 ?>
